01a626114b53b260f569c0171a9a97bac859468d
laith <laithalwani@gmail.com>
2023-10-20 11:20:44 -0400

    login route complete

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,8 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import { conntectToDB } from '@utils/database';
 
+import {User} from "@models/user"
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -9,11 +11,28 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) { 
+    const sessoinUser = await User.findOne({
+      email: session.user.email
+    })
+
+    session.user.id = sessoinUser._id.toString();
+    return session;
+  },
+  
   async signIn({ profile }) {
     try {
       await conntectToDB();
-
+      const userExsits = await User.findOne({
+        email: profile.email
+      });
+      if (!userExsits) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image:profile.image
+        })
+      }
       return true
     }
     catch (err) {
